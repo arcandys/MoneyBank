a902e09b1dda40cdff0f77ff52d424eb06208a29
arcandys <houstik01@icloud.com>
2023-10-12 14:01:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/moneybank/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.moneybank;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.ComponentActivity;
+
+public class MainActivity extends ComponentActivity {
+
+    private EditText username;
+    private EditText password;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainScreen();
+            }
+        });
+    }
+
+    public void openMainScreen(){
+        Intent intent = new Intent(this, MainScreen.class);
+        startActivity(intent);
+    }
+}
