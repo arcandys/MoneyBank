66cb9c02ffef54b3d21c541f45a088210f10e3ec
arcandys <houstik01@icloud.com>
2023-11-04 16:48:40 +0100

    updates to app
    Updates to Homepage + Navbar

--- app/src/main/java/com/example/moneybank/MainScreen.java
+++ app/src/main/java/com/example/moneybank/MainScreen.java
@@ -1,15 +1,39 @@
 package com.example.moneybank;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.moneybank.ui.theme.VirementsPage;
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainScreen extends AppCompatActivity {
+
+    private Button navButton1;
+    private Button navButton2;
+    private Button navButton3;
+    private Button navButton4;
+    private Button navButton5;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+    }
+
+    public void openAccueilActivity() {
+        Intent intent = new Intent(this, VirementsPage.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/moneybank/ui/theme/VirementsPage.java
@@ -0,0 +1,5 @@
+package com.example.moneybank.ui.theme;
+
+public class VirementsPage {
+
+}
